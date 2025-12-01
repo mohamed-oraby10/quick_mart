ec17303f6d93064ae2d05a895f5965a4678105d9
hp <mohamedoraby0500@gmail.com>
2025-12-01 23:35:45 +0200

    apply screen util on fonts

--- lib/core/utils/theme/theme_data/theme_data_dark.dart
+++ lib/core/utils/theme/theme_data/theme_data_dark.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:quick_mart/core/utils/app_colors.dart';
 import 'package:quick_mart/core/utils/styles.dart';
 
@@ -21,22 +22,22 @@ ThemeData getDarkTheme() {
         foregroundColor: AppColors.brandButtonText,
         disabledBackgroundColor: AppColors.greyDark50,
         disabledForegroundColor: AppColors.brandCyan,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.r)),
         textStyle: Styles.button2,
-        minimumSize: Size(double.infinity, 60),
+        minimumSize: Size(double.infinity, 60.h),
       ),
     ),
     inputDecorationTheme: InputDecorationTheme(
       border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(12),
+        borderRadius: BorderRadius.circular(12.r),
         borderSide: BorderSide(color: AppColors.greyDark50),
       ),
       enabledBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(12),
+        borderRadius: BorderRadius.circular(12.r),
         borderSide: BorderSide(color: AppColors.greyDark50),
       ),
       focusedBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(12),
+        borderRadius: BorderRadius.circular(12.r),
         borderSide: BorderSide(color: AppColors.brandCyan),
       ),
       fillColor: AppColors.brandBlack,
--- lib/core/utils/theme/theme_data/theme_data_light.dart
+++ lib/core/utils/theme/theme_data/theme_data_light.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:quick_mart/core/utils/app_colors.dart';
 import 'package:quick_mart/core/utils/styles.dart';
 
@@ -21,22 +22,22 @@ ThemeData getLightTheme() {
         foregroundColor: AppColors.brandButtonText,
         disabledBackgroundColor: AppColors.greyLight50,
         disabledForegroundColor: AppColors.brandButton,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.r)),
         textStyle: Styles.button2,
-        minimumSize: Size(double.infinity, 60),
+        minimumSize: Size(double.infinity, 60.h),
       ),
     ),
     inputDecorationTheme: InputDecorationTheme(
       border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(12),
+        borderRadius: BorderRadius.circular(12.r),
         borderSide: BorderSide(color: AppColors.greyLight50),
       ),
       enabledBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(12),
+        borderRadius: BorderRadius.circular(12.r),
         borderSide: BorderSide(color: AppColors.greyLight50),
       ),
       focusedBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(12),
+        borderRadius: BorderRadius.circular(12.r),
         borderSide: BorderSide(color: AppColors.brandCyan),
       ),
       fillColor: AppColors.brandWhite,
