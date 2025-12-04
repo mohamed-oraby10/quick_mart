207c07d570f696e70709df8e4d1dd74fe994afaf
hp <mohamedoraby0500@gmail.com>
2025-12-04 21:04:54 +0200

    finish email verification view

--- lib/Features/Auth/presentation/views/widgets/email_verification_text_section.dart
+++ lib/Features/Auth/presentation/views/widgets/email_verification_text_section.dart
@@ -2,16 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:quick_mart/Features/Auth/presentation/views/widgets/digits_code.dart';
 import 'package:quick_mart/core/utils/styles.dart';
+import 'package:quick_mart/core/widgets/custom_app_bar.dart';
 
-class EmailVerificationTextSection extends StatelessWidget {
+class EmailVerificationTextSection extends StatefulWidget {
   const EmailVerificationTextSection({super.key});
+
+  @override
+  State<EmailVerificationTextSection> createState() =>
+      _EmailVerificationTextSectionState();
+}
+
+class _EmailVerificationTextSectionState
+    extends State<EmailVerificationTextSection> {
+  bool isCodeSent = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 2), () {
+      setState(() {
+        isCodeSent = false;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        DigitsCode(),
+        AnimatedSwitcher(
+          duration: Duration(milliseconds: 600),
+          transitionBuilder: (child, animation) =>
+              FadeTransition(opacity: animation, child: child),
+          child: isCodeSent
+              ? DigitsCode()
+              : CustomAppBar(text: 'Verification Code'),
+        ),
         SizedBox(height: 22.h),
         Text('Email Verification', style: Styles.heading2Bold),
         SizedBox(height: 8.h),
