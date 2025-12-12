27101601b2b074231c14d9d43e645eb656b7a68d
hp <mohamedoraby0500@gmail.com>
2025-12-12 22:57:59 +0200

    create signup with email and password use case

---
+++ lib/core/use_cases/use_case.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:quick_mart/core/errors/failure.dart';
+
+abstract class UseCase<Type, Param> {
+  Future<Either<Failure, Type>> call([Param param]);
+}
+
+class NoParam {}
