8635b1b13d994fe38d8d7e784ea7b44e0de662ad
hp <mohamedoraby0500@gmail.com>
2025-12-13 00:35:40 +0200

    create app circular progress indicator

---
+++ lib/core/widgets/app_circular_progress_indicator.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:quick_mart/core/utils/app_colors.dart';
+import 'package:quick_mart/core/utils/theme/extensions/theme_extension.dart';
+
+class AppCircularProgressIndicator extends StatelessWidget {
+  const AppCircularProgressIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoActivityIndicator(
+      color: context.isDarkMode ? AppColors.brandCyan : AppColors.brandBlack,
+      radius: 14.r,
+    );
+  }
+}
