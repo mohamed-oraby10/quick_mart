09399d36cd25a46cec52fa13de27c13d276d352e
hp <mohamedoraby0500@gmail.com>
2025-12-13 21:46:59 +0200

    create auth repo impl

--- lib/Features/Auth/data/repos/auth_repo_impl.dart
+++ lib/Features/Auth/data/repos/auth_repo_impl.dart
@@ -1,43 +1,113 @@
 import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:quick_mart/Features/Auth/data/errors/auth_failure.dart';
 import 'package:quick_mart/Features/Auth/domain/entities/user_entity.dart';
 import 'package:quick_mart/Features/Auth/domain/repos/auth_repo.dart';
 import 'package:quick_mart/core/errors/failure.dart';
 
 class AuthRepoImpl extends AuthRepo {
+  
+  @override
+  Future<Either<AuthFailure, void>> loginWithEmailAndPassword({
+    required UserEntity userEntity,
+    required String password,
+  }) async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: userEntity.userEemail,
+        password: password,
+      );
+      return right(null);
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case 'invalid-email':
+          {
+            return Left(AuthFailure.invalidEmail());
+          }
+        case 'invalid-credential':
+          {
+            return Left(AuthFailure.invalidCredential());
+          }
+        default:
+          {
+            return Left(AuthFailure.unKnown());
+          }
+      }
+    }
+  }
+
+  @override
+  Future<Either<AuthFailure, void>> signupWithEmailAndPassword({
+    required UserEntity userEntity,
+    required String password,
+  }) async{
+     try {
+       await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: userEntity.userEemail, password: password);
+      // saveUserData(user: user);
+      return right(null);
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case 'weak-password':
+          {
+            return Left(AuthFailure.weakPassword());
+          }
+        case 'invalid-credential':
+          {
+            return Left(AuthFailure.invalidCredential());
+          }
+        case 'invalid-email':
+          {
+            return Left(AuthFailure.invalidEmail());
+          }
+        case 'email-already-in-use':
+          {
+            return Left(AuthFailure.usedEmail());
+          }
+        default:
+          return Left(AuthFailure.usedEmail());
+      }
+    }
+  }
+
+  @override
+  Future<Either<Failure, UserCredential>> loginWithGoogle() async {
+    try {
+      final GoogleSignIn googleSignIn = GoogleSignIn();
+      await googleSignIn.signOut();
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+      final credential = GoogleAuthProvider.credential(
+        idToken: googleAuth?.idToken,
+        accessToken: googleAuth?.accessToken,
+      );
+      UserCredential user = await FirebaseAuth.instance.signInWithCredential(
+        credential,
+      );
+      // saveUserData(user: user);
+      return Right(user);
+    } catch (e) {
+      return Left(AuthFailure.unKnown());
+    }
+  }
+  
+    @override
+  Future<Either<Failure, void>> saveUserData() {
+    // TODO: implement saveUserData
+    throw UnimplementedError();
+  }
+  
   @override
   Future<Either<Failure, void>> emailVerification() {
     // TODO: implement emailVerification
     throw UnimplementedError();
   }
-
-  @override
-  Future<Either<Failure, void>> loginWithEmailAndPassword({required UserEntity userEntity, required String password}) {
-    // TODO: implement loginWithEmailAndPassword
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Either<Failure, void>> loginWithGoogle() {
-    // TODO: implement loginWithGoogle
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Either<Failure, void>> saveUserData() {
-    // TODO: implement saveUserData
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Either<Failure, void>> signupWithEmailAndPassword({required UserEntity userEntity, required String password}) {
-    // TODO: implement signupWithEmailAndPassword
-    throw UnimplementedError();
-  }
-
+  
   @override
   Future<Either<Failure, void>> updatePassword() {
     // TODO: implement updatePassword
     throw UnimplementedError();
   }
- 
 }
--- lib/Features/Auth/domain/use_cases/login_with_google_use_case.dart
+++ lib/Features/Auth/domain/use_cases/login_with_google_use_case.dart
@@ -1,15 +1,16 @@
 import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:quick_mart/Features/Auth/domain/Repos/auth_repo.dart';
 import 'package:quick_mart/core/errors/failure.dart';
 import 'package:quick_mart/core/use_cases/no_parameter_use_case.dart';
 
-class LoginWithGoogleUseCase extends NoParameterUseCase<void> {
+class LoginWithGoogleUseCase extends NoParameterUseCase<UserCredential> {
   final AuthRepo authRepo;
 
   LoginWithGoogleUseCase(this.authRepo);
 
   @override
-  Future<Either<Failure, void>> call() async {
+  Future<Either<Failure, UserCredential>> call() async {
     return await authRepo.loginWithGoogle();
   }
 }
