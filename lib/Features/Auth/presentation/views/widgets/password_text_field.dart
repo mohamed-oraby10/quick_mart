1f32d3966e93c783a45974cbf0d1ab69ba348b79
hp <mohamedoraby0500@gmail.com>
2025-12-15 22:57:10 +0200

    enable obscure Text

--- lib/Features/Auth/presentation/views/widgets/password_text_field.dart
+++ lib/Features/Auth/presentation/views/widgets/password_text_field.dart
@@ -3,25 +3,37 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:icons_plus/icons_plus.dart';
 import 'package:quick_mart/core/utils/theme/extensions/theme_extension.dart';
 
-class PasswordTextField extends StatelessWidget {
+class PasswordTextField extends StatefulWidget {
   const PasswordTextField({super.key, this.controller});
   final TextEditingController? controller;
+
+  @override
+  State<PasswordTextField> createState() => _PasswordTextFieldState();
+}
+
+class _PasswordTextFieldState extends State<PasswordTextField> {
+  bool isObscure = false;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      obscureText: isObscure,
       validator: (value) {
         if (value!.isEmpty) {
           return 'Field is required';
         }
         return null;
       },
-      controller: controller,
+      controller: widget.controller,
       decoration: InputDecoration(
         hintText: 'Enter your password',
         suffixIcon: IconButton(
-          onPressed: () {},
+          onPressed: () {
+            setState(() {
+              isObscure = !isObscure;
+            });
+          },
           icon: Icon(
-            Iconsax.eye_outline,
+            isObscure ? Iconsax.eye_slash_outline : Iconsax.eye_outline,
             size: 24.sp,
             color: context.customColors.modeColor,
           ),
