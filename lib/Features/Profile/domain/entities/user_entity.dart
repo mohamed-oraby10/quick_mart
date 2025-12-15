02e4fe4fe78bd5e03efe271bb4ba12d2084ad3a6
hp <mohamedoraby0500@gmail.com>
2025-12-15 14:13:43 +0200

    generate user entity adapter

--- lib/Features/Profile/domain/entities/user_entity.dart
+++ lib/Features/Profile/domain/entities/user_entity.dart
@@ -1,7 +1,16 @@
+import 'package:hive/hive.dart';
+
+part 'user_entity.g.dart';
+
+@HiveType(typeId: 0)
 class UserEntity {
+  @HiveField(0)
   final String userIdd;
+  @HiveField(1)
   final String fullName;
+  @HiveField(2)
   final String userEemail;
+  @HiveField(3)
   final String? image;
   UserEntity({
     required this.fullName,
