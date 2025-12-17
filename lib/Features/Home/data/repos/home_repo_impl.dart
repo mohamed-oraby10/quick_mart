ba75935afb45c5ef438efaabe0e1d2ff0f4de5e6
hp <mohamedoraby0500@gmail.com>
2025-12-17 22:04:36 +0200

    create Home Repo Impl

---
+++ lib/Features/Home/data/repos/home_repo_impl.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:quick_mart/Features/Home/domain/entities/category_entity.dart';
+import 'package:quick_mart/Features/Home/domain/entities/product_entity.dart';
+import 'package:quick_mart/Features/Home/domain/repos/home_repo.dart';
+import 'package:quick_mart/core/errors/failure.dart';
+
+class HomeRepoImpl extends HomeRepo{
+  @override
+  Future<Either<Failure, List<CategoryEntity>>> fetchCategories({required int pageNumber}) {
+    // TODO: implement fetchCategories
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<ProductEntity>>> fetchLeatestProducts({required int pageNumber}) {
+    // TODO: implement fetchLeatestProducts
+    throw UnimplementedError();
+  }
+}
