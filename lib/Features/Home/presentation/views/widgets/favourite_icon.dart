bca3b2dd758ce399dd6df9bc64d721b532c3a855
hp <mohamedoraby0500@gmail.com>
2025-12-20 21:54:32 +0200

    enable Favourite Icon

--- lib/Features/Home/presentation/views/widgets/favourite_icon.dart
+++ lib/Features/Home/presentation/views/widgets/favourite_icon.dart
@@ -4,9 +4,15 @@ import 'package:icons_plus/icons_plus.dart';
 import 'package:quick_mart/core/utils/app_colors.dart';
 import 'package:quick_mart/core/utils/theme/extensions/theme_extension.dart';
 
-class FavouriteIcon extends StatelessWidget {
-  const FavouriteIcon({super.key, this.onTap});
-  final void Function()? onTap;
+class FavouriteIcon extends StatefulWidget {
+  const FavouriteIcon({super.key});
+
+  @override
+  State<FavouriteIcon> createState() => _FavouriteIconState();
+}
+
+class _FavouriteIconState extends State<FavouriteIcon> {
+  bool isFavourite = false;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -20,12 +26,20 @@ class FavouriteIcon extends StatelessWidget {
         ),
         child: Center(
           child: GestureDetector(
-            onTap: onTap,
+            onTap: () {
+              setState(() {
+                isFavourite = !isFavourite;
+              });
+            },
             child: Icon(
-              Iconsax.heart_outline,
+              isFavourite ? Iconsax.heart_bold : Iconsax.heart_outline,
               size: 12.sp,
               color: context.isDarkMode
-                  ? AppColors.brandBlack
+                  ? isFavourite
+                        ? AppColors.generalRed
+                        : AppColors.brandBlack
+                  : isFavourite
+                  ? AppColors.generalRed
                   : AppColors.brandWhite,
             ),
           ),
