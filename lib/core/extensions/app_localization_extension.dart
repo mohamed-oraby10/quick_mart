f5aa131d305ba86651e4bfe73131771810a04057
hp <mohamedoraby0500@gmail.com>
2026-01-06 14:16:49 +0200

    add isArabic extension

--- lib/core/extensions/app_localization_extension.dart
+++ lib/core/extensions/app_localization_extension.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quick_mart/core/cubits/language_cubit/language_cubit.dart';
 import 'package:quick_mart/l10n/app_localizations.dart';
 
 extension AppLocalization on BuildContext {
   AppLocalizations get locale => AppLocalizations.of(this)!;
+  bool get isArabic => watch<LanguageCubit>().state.languageCode == 'ar';
 }
--- lib/core/widgets/arrow_left_icon.dart
+++ lib/core/widgets/arrow_left_icon.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:icons_plus/icons_plus.dart';
-import 'package:quick_mart/core/cubits/language_cubit/language_cubit.dart';
+import 'package:quick_mart/core/extensions/app_localization_extension.dart';
 import 'package:quick_mart/core/utils/theme/extensions/theme_extension.dart';
 
 class ArrowLeftIcon extends StatelessWidget {
@@ -16,16 +15,12 @@ class ArrowLeftIcon extends StatelessWidget {
       onPressed: () {
         GoRouter.of(context).pop();
       },
-      icon: BlocBuilder<LanguageCubit, Locale>(
-        builder: (context, locale) {
-          return Icon(
-            locale.languageCode == 'ar'
-                ? Iconsax.arrow_right_1_outline
-                : Iconsax.arrow_left_outline,
-            size: 32.sp,
-            color: context.customColors.modeColor,
-          );
-        },
+      icon: Icon(
+        context.isArabic
+            ? Iconsax.arrow_right_1_outline
+            : Iconsax.arrow_left_outline,
+        size: 32.sp,
+        color: context.customColors.modeColor,
       ),
     );
   }
