187d29ff2e92b1dbca57197077582edb525ff206
hp <mohamedoraby0500@gmail.com>
2026-02-13 14:59:49 +0200

    provide Fetch Ongoing Orders Cubit to view

--- lib/Features/Profile/presentation/views/order_history_view.dart
+++ lib/Features/Profile/presentation/views/order_history_view.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quick_mart/Features/Profile/data/repos/profile_repo_impl.dart';
+import 'package:quick_mart/Features/Profile/domain/use_cases/fetch_ongoing_orders_use_case.dart';
+import 'package:quick_mart/Features/Profile/presentation/manager/fetch_ongoing_orders_cubit/fetch_ongoing_orders_cubit.dart';
 import 'package:quick_mart/Features/Profile/presentation/views/widgets/order_history_view_body.dart';
+import 'package:quick_mart/core/utils/functions/setup_service_locator.dart';
 
 class OrderHistoryView extends StatelessWidget {
   const OrderHistoryView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: const Scaffold(body: SafeArea(child: OrderHistoryViewBody())),
+    return BlocProvider(
+      create: (context) => FetchOngoingOrdersCubit(
+        FetchOngoingOrdersUseCase(getIt.get<ProfileRepoImpl>()),
+      )..fetchOngoingOrders(),
+      child: DefaultTabController(
+        length: 2,
+        child: const Scaffold(body: SafeArea(child: OrderHistoryViewBody())),
+      ),
     );
   }
 }
